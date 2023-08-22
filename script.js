a0995c1468be9f3a574169278476b710b5c94495
Alfath Izha Barikallah <alfathizhaaaaaaa@gmail.com>
2023-08-22 13:18:05 +0700

    toggle navbar fix

--- script.js
+++
@@ -1,6 +0,0 @@
-    const mobileMenuBtn = document.getElementById("MobileMenuBtn");
-    const mobile = document.getElementById("mobile-menu");
-
-    mobileMenuBtn.addEventListener("click", () => {
-        mobile.classList.toggle("translate-x-0");
-    });
