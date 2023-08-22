9470e678bcaadee74f287fe808ac4f7bd52ff827
Alfath Izha Barikallah <alfathizhaaaaaaa@gmail.com>
2023-08-22 14:30:19 +0700

    font inter setup

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,11 @@
 module.exports = {
   content: ["./src/**/*.{html,js}","./*.html"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily : {
+        'font-inter': ['Inter','sans-serif']
+      }
+    },
   },
   plugins: [],
 }
