a20a03306034fe6a3837cb71ae0b4a2a6e291e96
Alfath Izha Barikallah <alfathizhaaaaaaa@gmail.com>
2023-10-03 16:56:04 +0700

    final commit

---
+++ src/modul_daftar_pasien_yang_masih_dirawat/tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      colors: {
+        biru : '#41abf2',
+        abu :'#363940'
+      },
+    },
+
+  },
+  plugins: [],
+}
